283ef98c7d15c6d2d724cde2149120fbfa337321
Maksim <wolker2015@gmail.com>
2024-09-08 16:16:08 +0700

    added pipes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame as pg
 
 pg.init()
@@ -30,7 +32,36 @@ class Bird(pg.sprite.Sprite):
     def jump(self):
         self.velocity = self.lift
 
+class Pipe(pg.sprite.Sprite):
+    TOP = 1
+    BOTTOM = 2
+    def __init__(self, type, gap_start):
+        super().__init__()
+        self.image = pg.image.load('res/pipe.png')
+        self.image = pg.transform.scale(self.image, [PIPE_WIDTH, self.image.get_height() / 2])
+        if type == self.TOP:
+            self.image = pg.transform.flip(self.image, False, True)
+            self.rect = self.image.get_rect(bottomleft=(SCREEN_WIDTH, gap_start))
+        elif type == self.BOTTOM:
+            self.rect = self.image.get_rect(topleft=(SCREEN_WIDTH, gap_start + PIPE_GAP))
+            self.passed = False
+    def update(self):
+        self.rect.x -= SPEED
+        if self.rect.right < 0:
+            self.kill()
+
+
 bird = Bird()
+pipes = pg.sprite.Group()
+
+def make_pipes():
+    gap_start = random.randint(50, SCREEN_HEIGHT - PIPE_GAP - 50)
+    top_pipe = Pipe(Pipe.TOP, gap_start)
+    bottom_pipe = Pipe(Pipe.BOTTOM, gap_start)
+    pipes.add(top_pipe, bottom_pipe)
+
+
+make_pipes()
 
 def main():
     while True:
@@ -49,6 +80,7 @@ def main():
         screen.blit(bird.image, bird.rect)
         pg.display.update()
         pg.time.delay(30)
+
 
 if __name__ == '__main__':
     main()
